24849f25a28dc050c54a14f2d3bb89982de66caf
cristin <cristin.herrera.guerra@gmail.com>
2018-10-25 19:27:38 -0500

    segundo

--- solution.rb
+++ solution.rb
@@ -4,9 +4,9 @@ require 'sinatra'
 
 get '/' do
   name = params["name"]
-  if name
+  if name && name != ""
      "<h1>Hola #{name}!</h1>"
-  else
+   else
      "<h1>Hola desconocido!<h1>"
   end
 end
